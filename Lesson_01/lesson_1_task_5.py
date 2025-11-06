2d4cfd3bf5aa5448683609c5bf628af1dc45ecdb
alenka-zubina@yandex.ru <alenka-zubina@yandex.ru>
2025-11-06 15:24:56 +0300

    first commit

---
+++ Lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def func_num(num):
+    print(num)
+
+
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
+func_num(88005553535)
